e3d661afb920a5d9b318e30dedf5e3ea1f3d8191
lijovijayan <lijovijayan00@gmail.com>
2023-11-14 12:17:34 +0530

    aruco tags | added tag number & border

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-(function() {
+(function () {
 
     var $marker_count = $('#marker-count');
     var $marker_random = $('#marker-random:checkbox');
@@ -17,43 +17,48 @@
 
     var MAX_CODES = 1024;
 
-    var rand_num = function(max) {
+    var rand_num = function (max) {
         var r = Math.random();
         return Math.floor(r * max);
     };
 
-    var parse = function(text) {
+    var parse = function (text) {
 
         var values = text.replace(' ', '')
             .split(',')
-            .filter(function(d) {
+            .filter(function (d) {
                 return d.length !== 0;
             })
-            .map(function(d) {
+            .map(function (d) {
                 return Number(d, 10);
             })
-            .filter(function(d) {
+            .filter(function (d) {
                 return d === 0 || d;
             });
 
         return values;
     };
 
-    var create_and_append_markers = function(ids) {
+    var create_and_append_markers = function (ids) {
         $markers.html('');
-        ids.forEach(function(id) {
+        ids.forEach(function (id) {
             var marker = new ArucoMarker(id);
+            var $markerContainer = $('<div></div>').addClass('marker-container')
+                .css('padding', spacing)
+                .css('flex-direction', 'column');
             var $marker = $('<div></div>')
                 .addClass('marker')
                 .css('width', width)
-                .css('padding-bottom', spacing)
-                .css('padding-left', spacing);
-            $marker.html(marker.toSVG());
-            $markers.append($marker);
+                .html(marker.toSVG());
+            var $idLabel = $('<div></div>')
+                .addClass('id-label')
+                .text(id);
+            $markerContainer.append($marker, $idLabel);
+            $markers.append($markerContainer);
         });
     };
 
-    var marker_count_callback = function() {
+    var marker_count_callback = function () {
         var num = Number($marker_count.val());
         var random = $marker_random.is(':checked');
 
@@ -71,7 +76,7 @@
         create_and_append_markers(ids);
     };
 
-    var marker_ids_callback = function() {
+    var marker_ids_callback = function () {
 
         // try to parse the input
         var text = $marker_ids.val();
@@ -84,7 +89,7 @@
 
     };
 
-    var marker_width_callback = function() {
+    var marker_width_callback = function () {
         var num = Number($marker_width.val()) + 'cm';
         if (num) {
             width = num;
@@ -92,11 +97,11 @@
         }
     };
 
-    var marker_spacing_callback = function() {
+    var marker_spacing_callback = function () {
         var num = Number($marker_spacing.val()) + 'cm';
         if (num) {
             spacing = num;
-            $('.marker').css('padding-bottom', num).css('padding-left', num);
+            $('.marker-container').css('padding', num);
         }
     };
 
